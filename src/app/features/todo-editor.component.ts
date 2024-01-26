d36ebdfea59e6949a850ddd17f7ca9e90a7232a9
Djordje Jovanovic <djordje.jovanoviic@gmail.com>
2024-01-26 15:26:49 +0100

    Create addTodo method

--- src/app/features/todo-editor.component.ts
+++ src/app/features/todo-editor.component.ts
@@ -1,17 +1,45 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  ViewChild,
+  inject,
+} from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { TodoService } from '../core/todos.service';
+import TodoItemComponent from '../shared/todo-item.component';
 
 @Component({
   standalone: true,
   selector: 'todo-editor',
   template: `
-    <div>
-      <input
-        class="border-2 rounded-lg  mt-3 text-gray-900 p-5 w-full"
-        type="text"
-        placeholder="Enter a task..."
-      />
-    </div>
+    <input
+      #input
+      class="border-2 rounded-lg  mt-3 text-gray-900 p-5 w-full"
+      type="text"
+      placeholder="Enter a task..."
+      (blur)="addTodo()"
+    />
+
+    @for (todo of todos(); track $index) {
+    <todo-item [todo]="todo" />
+    }
   `,
+  imports: [TodoItemComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TodoEditorComponent {}
+export class TodoEditorComponent {
+  @ViewChild('input') inputTodo!: ElementRef;
+
+  private todoService = inject(TodoService);
+
+  todos = toSignal(this.todoService.getTodos());
+
+  addTodo() {
+    this.todoService
+      .addTodos(this.inputTodo.nativeElement.value)
+      .subscribe(() => {
+        console.log('Added todo');
+      });
+  }
+}
--- src/app/shared/todo-item.component.ts
+++ src/app/shared/todo-item.component.ts
@@ -4,7 +4,15 @@ import { Todo } from '../core/todo.model';
 @Component({
   standalone: true,
   selector: 'todo-item',
-  template: ``,
+  template: `
+    <div class="flex items-center">
+      <div
+        class="border-2 rounded-lg bg-neutral-50 text-gray-900  mt-3  p-5 w-full"
+      >
+        {{ todo.title }}
+      </div>
+    </div>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class TodoItemComponent {
